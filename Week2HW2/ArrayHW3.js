63daa8df26fa45ab825f314310a017d987e1ac2d
Samuel Kyere <samuelboatengkyere@yahoo.com>
2021-12-13 11:50:11 -0500

    Assignment on Functions

---
+++ Week2HW2/ArrayHW3.js
@@ -0,0 +1,12 @@
+const ballondor = ['Lewandowski',' M. Salah','Ngolo Kante','Edouard Mendy']
+let longest = '';
+for (i=0; i<ballondor.length;i++){
+    if(ballondor[i].length > longest.length) {
+        longest = ballondor[i]
+    
+    }
+}
+console.log(longest)
+{
+    
+}
